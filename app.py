62113d3529dc9a537883b27688637cd855bbb09c
INDRAJIT SINGH <Indrajitsingh37@gmail.com>
2022-03-09 15:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def pdf_Reader(pdf_document):
         print(page1.extractText())
         
 text = pdf_Reader(pdf_document)
-st.write(text)
+
 
 select_context = """Ramakrishna, the God-man of modern times, was born on February 18, 1836, in the
 little village of Kamarpukur, in the district of Hooghly in Bengal. How different were
@@ -122,7 +122,7 @@ Brahman as the immutable essence of all things.
 
 """
 
-context = st.text_area("select a context",select_context)
+context = st.text_area("select a context",text)
 options = st.multiselect(
      'Select question from Below List',
      ['Who am I?', 'Where am I going to?', "What's the purpose of Life ?", 'What do others mean to me?',"Where does Narendra's family lived?"])
